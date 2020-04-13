f3bd51fa995f66eb26a6bb4d422e5ca181d33011
Kenneth Obikwelu <obikwelukenneth@gmail.com>
2020-04-12 23:19:22 -0700

    major update

--- index.php
+++ index.php
@@ -1,1 +1,245 @@
-<?php include_once(“home.html”); ?>
+<!DOCTYPE html>
+<!-- saved from url=(0052)http://themestarz.net/html/innovo/3-text-sphere.html -->
+<html lang="en" class=" preserve-3d js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers no-applicationcache svg inlinesvg smil svgclippaths"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style>.gm-control-active>img{box-sizing:content-box;display:none;left:50%;pointer-events:none;position:absolute;top:50%;transform:translate(-50%,-50%)}.gm-control-active>img:nth-child(1){display:block}.gm-control-active:hover>img:nth-child(1),.gm-control-active:active>img:nth-child(1){display:none}.gm-control-active:hover>img:nth-child(2),.gm-control-active:active>img:nth-child(3){display:block}
+</style><link type="text/css" rel="stylesheet" href="./assets/css"><style>.gm-ui-hover-effect{opacity:.6}.gm-ui-hover-effect:hover{opacity:1}
+</style><style>.gm-style .gm-style-cc span,.gm-style .gm-style-cc a,.gm-style .gm-style-mtc div{font-size:10px;box-sizing:border-box}
+</style><style>@media print {  .gm-style .gmnoprint, .gmnoprint {    display:none  }}@media screen {  .gm-style .gmnoscreen, .gmnoscreen {    display:none  }}</style><style>.dismissButton{background-color:#fff;border:1px solid #dadce0;color:#1a73e8;border-radius:4px;font-family:Roboto,sans-serif;font-size:14px;height:36px;cursor:pointer;padding:0 24px}.dismissButton:hover{background-color:rgba(66,133,244,0.04);border:1px solid #d2e3fc}.dismissButton:focus{background-color:rgba(66,133,244,0.12);border:1px solid #d2e3fc;outline:0}.dismissButton:hover:focus{background-color:rgba(66,133,244,0.16);border:1px solid #d2e2fd}.dismissButton:active{background-color:rgba(66,133,244,0.16);border:1px solid #d2e2fd;box-shadow:0 1px 2px 0 rgba(60,64,67,0.3),0 1px 3px 1px rgba(60,64,67,0.15)}.dismissButton:disabled{background-color:#fff;border:1px solid #f1f3f4;color:#3c4043}
+</style><style>.gm-style-pbc{transition:opacity ease-in-out;background-color:rgba(0,0,0,0.45);text-align:center}.gm-style-pbt{font-size:22px;color:white;font-family:Roboto,Arial,sans-serif;position:relative;margin:0;top:50%;-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%);transform:translateY(-50%)}
+</style><style>.gm-style img{max-width: none;}.gm-style {font: 400 11px Roboto, Arial, sans-serif; text-decoration: none;}</style>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="ThemeStarz">
+
+    <link rel="stylesheet" href="./assets/css(1)">
+    <link rel="stylesheet" href="./assets/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="./assets/font-awesome.css" type="text/css">
+    <link rel="stylesheet" href="./assets/parallax-hero.css">
+    <link rel="stylesheet" href="./assets/magnific-popup.css">
+    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/user.css">
+    <link rel="shortcut icon" href="./assets/favicon.png" />
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+
+	<title>Pintellect | Code Visualization | Code Documentation | Code Interaction | Code Engagement Platform</title>
+
+<script type="text/javascript" charset="UTF-8" src="./assets/common.js"></script><script type="text/javascript" charset="UTF-8" src="./assets/util.js"></script><script type="text/javascript" charset="UTF-8" src="./assets/map.js"></script><script type="text/javascript" charset="UTF-8" src="./assets/marker.js"></script><script type="text/javascript" charset="UTF-8" src="./assets/controls.js"></script><script type="text/javascript" charset="UTF-8" src="./assets/stats.js"></script><script type="text/javascript" charset="UTF-8" src="./assets/onion.js"></script></head>
+<body class="side-panel-light has-loading-screen color-theme-pink  expansion-alids-init loading-done pace-done hide-loading-screen" data-background-color="#1F1423" style="background-color: #1F1423;" data-gr-c-s-loaded="true"><div class="pace  pace-inactive"><div class="pace-progress" data-progress-text="100%" data-progress="99" style="transform: translate3d(100%, 0px, 0px);">
+  <div class="pace-progress-inner"></div>
+</div>
+<div class="pace-activity"></div></div>
+
+    <!--PAGE-->
+
+	<div id="page">
+		<div class="page-wrapper">
+			<figure class="page-wrapper-inner">
+				<div class="base-layer">
+                    <canvas id="displayCanvas" width="1540" height="862"></canvas>
+                </div>
+				<!--end layer-->
+				<div class="layer animate pointer-events-none show-it">
+                    <div id="particles-js"></div>
+                </div>
+				<!--end layer-->
+				<div class="layer animate show-it">
+					<div id="content">
+						<div class="content-wrapper">
+							<div class="container">
+                                <div class="brand">
+                                    <a href="#">
+                                        <img src="./assets/logo.svg" alt="">
+                                    </a>
+                                </div>
+                                <!--end brand-->
+								<div class="heading">
+									<h1 class="large">A.I. based code visualization</h1>
+                                    <p>Learn, explore, understand and visualize code like a pro!
+</p>
+								</div>
+								<!--end heading-->
+								<a href="http://themestarz.net/html/innovo/3-text-sphere.html#" class="btn btn-primary" data-toggle="modal" data-target="#modal-subscribe">Notify Me</a>
+								<a href="http://themestarz.net/html/innovo/3-text-sphere.html#" class="btn btn-border open-side-panel">More Information</a>
+							</div>
+							<!--end container-->
+						</div>
+						<!--end content-wrapper-->
+					</div>
+					<!--end content-->
+				</div>
+				<!--end layer-->
+			</figure>
+			<!--end cd-floating-background-->
+		</div>
+		<!--end cd-background-wrapper-->
+	</div>
+	<!--end page-->
+
+    <!--SIDE PANEL-->
+
+    <div class="side-panel">
+        <div class="nav-btn open-side-panel">
+            <i></i>
+            <i></i>
+            <i></i>
+        </div>
+        <div class="wrapper">
+            <div class="container-fluid">
+                <section>
+                    <h2><img src="assets/logo+text+white.png" style="width: 50%"></h2>
+                    <p>
+                        Pintellect is an interactive code visualization and documentation platform that leverages the science of machine learning (ML), artificial intelligence (AI) and natural language processing (NLP) to make it easier for anyone to read, understand and document the structure, purpose and flow of any codebase. The following are key features of the platform:
+
+                    </p>
+                    <div class="gallery">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6">
+                                <div class="image">
+                                   
+                                    <h4>Code Visualizatioon</h4>
+                                    <p>Learn and understand the flow of the codebase through an interactive visualization tool to identify dependencies faster.</p>
+                                </div>
+                                <!--end image-->
+                            </div>
+                            <!--end col-md-6-->
+                            <div class="col-md-6 col-sm-6">
+                                <div class="image">
+                                    
+                                    <h4>Code Documentation</h4>
+                                    <p>Review reliable documentation through auto, AI-based documentation happening in real-time, associated with each commit ID. </p>
+                                </div>
+                                <!--end image-->
+                            </div>
+                            <!--end col-md-6-->
+                            <div class="col-md-6 col-sm-6">
+                                <div class="image">
+                                    
+                                    <h4>Code Interaction</h4>
+                                    <p>Build code walkthroughs and enable highlighting and annotations of the code within the developer's environment.</p>
+                                </div>
+                                <!--end image-->
+                            </div>
+                            <!--end col-md-6-->
+                            <div class="col-md-6 col-sm-6">
+                                <div class="image">
+                                    
+                                    <h4>Code Engagement</h4>
+                                    <p>Share and engage on the code easier and more efficiently through Slack and Microsoft Teams integrations.</p>
+                                </div>
+                                <!--end image-->
+                            </div>
+                            <!--end col-md-6-->
+                        </div>
+                    </div>
+                </section>
+
+      
+         
+                <p class="copyright">&copy; 2020 Pintellect. All Rights Reserved.</p>
+            </div>
+            <!--container-fluid-->
+        </div>
+    </div>
+    <!--end side-panel-->
+
+    <!--SUBSCRIBE MODAL-->
+
+	<div class="modal fade" id="modal-subscribe" tabindex="-1" role="dialog" aria-labelledby="modal-subscribe-label">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content" data-background-image="assets/img/pattern.png" style="background-image: url(&quot;assets/img/pattern.png&quot;);">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+					<h2 class="modal-title" id="modal-subscribe-label">Stay Tuned</h2>
+				</div>
+                <!--end modal-header-->
+				<div class="modal-body">
+				    <div class="alert alert-danger display-error " style="display: none">
+    </div>
+	 <div class="alert alert-success display-success " style="display: none">
+    </div>
+	
+					<p>
+						Be the first to know fresh news, updates and new releases! Just add your e-mail address and well
+						let you know.
+					</p>
+				</div>
+                <!--end modal-body-->
+				<div class="modal-footer" >
+					<form class="form"  role="form" id="contactForm" >
+						<div class="form-group">
+							<label for="subscribe-email">Email address</label>
+							<input type="email" class="form-control" name="email" id="email" placeholder="Email Address" required="">
+						</div>
+						<button type="submit" id="submit" class="btn btn-primary">Submit</button>
+					</form>
+				</div>
+                <!--modal-footer-->
+			</div>
+            <!--modal-content-->
+		</div>
+        <!--modal-dialog-->
+	</div>
+	<!--end modal-->
+ <script type="text/javascript">
+  $(document).ready(function() {
+      $('#submit').click(function(e){
+        e.preventDefault();
+        var email = $("#email").val();
+        $.ajax({
+            type: "POST",
+            url: "https://pintel-api-node.herokuapp.com/api/v1/notifyMe",
+            dataType: "json",
+            data: { email:email},
+            success : function(data){
+                console.log(data)
+                if (data.message == "Success"){
+                   $(".display-success").html("<p style={color:'green'} >"+'Success'+"</p>");
+                    $(".display-success").css("display","block");
+                    $("#email").val('');
+
+                } else {
+                    $(".display-error").html("<ul>"+data.msg+"</ul>");
+                    $(".display-error").css("display","block");
+                    $("#email").val('');
+
+                }
+
+                
+            }
+        });
+
+
+      });
+  });
+</script
+	<script src="./assets/jquery-2.1.1.js"></script>
+	<script type="text/javascript" src="./assets/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./assets/js"></script>
+    <script src="./assets/pace.min.js"></script>
+    <script src="./assets/jquery.validate.min.js"></script>
+	<script src="./assets/parallax-hero.js"></script>
+	<script src="./assets/modernizr.js"></script>
+	<script src="./assets/jquery.magnific-popup.min.js"></script>
+	<script src="./assets/particle-sphere.js"></script>
+	<script src="./assets/custom.js"></script>
+
+	<script type="text/javascript">
+
+        // RGB Color
+        var r = 47;
+        var g = 28;
+        var b = 52;
+        var backgroundColor = "#1F1423";
+        window.addEventListener("load", particleSphree(r,g,b, backgroundColor), false);
+
+        var latitude = 34.038405;
+        var longitude = -117.946944;
+        var markerImage = "assets/img/map-marker.png";
+        var mapTheme = "light";
+        var mapElement = "map-contact";
+        google.maps.event.addDomListener(window, 'load', simpleMap(latitude, longitude, markerImage, mapTheme, mapElement));
+
+	</script>
+
+
+
+<div class="loader"></div></body></html>
